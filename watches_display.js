a0a2d5f391bb4816973051ecc5e3d6a989c72a53
Med.Charfeddine <charfedchaibi@gmail.com>
2024-07-08 11:55:46 +0100

    WIP

---
+++ watches_display.js
@@ -0,0 +1,23 @@
+function factoryWatch(name,type,color,price){
+var watch ={}
+    
+watch.name = name,
+watch.type = type,
+watch.color = color, 
+watch.price = price,
+watch.display = display,
+watch.add = addtoCart
+
+return watch
+
+}
+
+
+
+function display() {
+
+}
+
+function addtoCart(){
+    
+}
